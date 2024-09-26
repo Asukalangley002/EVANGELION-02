e004eb1f3dbbee2ac60f2ff7be502e7ab0f4276b
兰格雷明日香 <asukallangley002@gmail.com>
2024-09-26 22:19:58 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	printf("      *\n");
+	printf("    *****\n");
+	printf("     ***\n");
+	printf("     | |\n");
+	return 0;
+}
---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,6 @@
+﻿#include <stdio.h>
+int main()
+{
+	printf("hello would");
+	return 0;
+}
